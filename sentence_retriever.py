4b9d60d1f1fbd5149ab52f0b4959fea7785c19a4
Roman Fazili <r.d.fazili@gmail.com>
2026-03-17 16:58:06 +0100

    Implement BM25, add Tokenizer support

--- sentence_retriever.py
+++ sentence_retriever.py
@@ -1,13 +1,44 @@
 from data_set import DataSet
+from rank_bm25 import BM25Okapi
+import numpy as np
+from typing import Callable
+from dotenv import load_dotenv
+import os
+
+
+Tokenizer = Callable[[str], list[str]]
+
+
+def default_tokenizer(text: str) -> list[str]:
+    return text.lower().split()
 
 
 class SentenceRetriever:
 
-    def __init__(self, data_set: DataSet):
+    def __init__(self, data_set: DataSet, tokenizer: Tokenizer | None = None):
         self.data_set = data_set
+        self._tokenizer: Tokenizer = tokenizer or default_tokenizer
+
+    def tokenize(self, text: str) -> list[str]:
+        return self._tokenizer(text)
 
     def BM25_demonstration_selection(self, query_sentence: str, top_k: int):
-        raise NotImplementedError("BM25 demonstration selection is not implemented")
+
+        all_sentences: list[str] = self.data_set.all_sentences_as_text
+
+        tokenized_train_data: list[list[str]] = [
+            self.tokenize(sentence) for sentence in all_sentences
+        ]
+        tokenized_query: list[str] = self.tokenize(query_sentence)
+
+        bm25 = BM25Okapi(corpus=tokenized_train_data)
+
+        scores: np.ndarray = bm25.get_scores(tokenized_query)
+
+        # Invert and get the k most similar sentences
+        top_indices: np.ndarray = scores.argsort()[-top_k:][::-1]
+
+        return [all_sentences[i] for i in top_indices]
 
     def SimCSE_demonstration_selection(self, query_sentence: str, top_k: int):
         raise NotImplementedError("SimCSE demonstration selection is not implemented")
@@ -15,8 +46,9 @@ class SentenceRetriever:
 
 
 if __name__ == "__main__":
-    file_path = input("Enter the path to the XML file: ")
+    load_dotenv()
+    file_path = os.getenv("PATH_TO_SEMEVAL_16_TRAIN_DATA")
     data_set = DataSet(file_path)
     sentence_retriever = SentenceRetriever(data_set)
     print(sentence_retriever.BM25_demonstration_selection("The food was good", 3))
-    print(sentence_retriever.SimCSE_demonstration_selection("The food was good", 3))
+    # print(sentence_retriever.SimCSE_demonstration_selection("The food was good", 3))
