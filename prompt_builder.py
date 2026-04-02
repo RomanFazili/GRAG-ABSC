9a594fe0f5b85c9383f9edf4f0413f5ebb73218f
Roman Fazili <r.d.fazili@gmail.com>
2026-04-02 22:47:04 +0200

    Reformatted the class

--- prompt_builder.py
+++ prompt_builder.py
@@ -59,6 +59,38 @@ class PromptBuilder:
 
         sentence_retriever = SentenceRetriever(DataSet(train_data_filepath))
 
+        formatted_demonstrations = PromptBuilder._format_demonstrations(
+            demonstration_selection_method, 
+            top_k, 
+            sentence_retriever, 
+            input_sentence
+        )
+
+        ontology_retriever = OntologyRetriever(DataSetOntology(ontology_filepath))
+
+        formatted_ontology = PromptBuilder._format_ontology(
+            ontology_retriever,
+            ontology_selection_method, 
+            ontology_format
+        )
+
+        prompt = PromptBuilder._build_prompt(
+            input_sentence=input_sentence,
+            aspect=aspect,
+            formatted_demonstrations=formatted_demonstrations,
+            formatted_ontology=formatted_ontology,
+        )
+
+        return prompt
+
+    @staticmethod
+    def _format_demonstrations(
+        demonstration_selection_method: DemonstrationSelectionMethod,
+        top_k: int,
+        sentence_retriever: SentenceRetriever,
+        input_sentence: str,
+    ) -> str | None:
+
         demonstration_sentences: list[tuple[str, list[tuple[str, Polarity]]]] = []
 
         if demonstration_selection_method == DemonstrationSelectionMethod.BM25:
@@ -75,10 +107,22 @@ class PromptBuilder:
                 input_sentence, top_k
             )
 
-        formatted_demonstrations = PromptBuilder._format_demonstrations(demonstration_sentences)
+        if not demonstration_sentences:
+            return None
 
-        data_set_ontology = DataSetOntology(ontology_filepath)
-        ontology_retriever = OntologyRetriever(data_set_ontology)
+        return "\n".join(
+            [
+                f"Sentence: {sentence}\nAspects and Polarities: {[(aspect, str(polarity)) for aspect, polarity in aspects_and_polarities]}"
+                for sentence, aspects_and_polarities in demonstration_sentences
+            ]
+        )
+
+    @staticmethod
+    def _format_ontology(
+        ontology_retriever: OntologyRetriever,
+        ontology_selection_method: OntologySelectionMethod,
+        ontology_format: OntologyFormat,
+    ) -> str | None:
 
         selected_ontology: Graph | None = None
         if ontology_selection_method == OntologySelectionMethod.Nothing:
@@ -89,40 +133,15 @@ class PromptBuilder:
         elif ontology_selection_method == OntologySelectionMethod.Full:
             selected_ontology = ontology_retriever.data_set_ontology.get_rdflib_graph()
 
-        
-        formatted_ontology: str | None = None
         if selected_ontology:
             if ontology_format == OntologyFormat.XML:
-                formatted_ontology = selected_ontology.serialize(format="xml")
+                return selected_ontology.serialize(format="xml")
             elif ontology_format == OntologyFormat.N3:
-                formatted_ontology = selected_ontology.serialize(format="n3")
+                return selected_ontology.serialize(format="n3")
             elif ontology_format == OntologyFormat.NT:
-                formatted_ontology = selected_ontology.serialize(format="nt")
+                return selected_ontology.serialize(format="nt")
 
-
-        prompt = PromptBuilder._build_prompt(
-            input_sentence=input_sentence,
-            aspect=aspect,
-            formatted_demonstrations=formatted_demonstrations,
-            formatted_ontology=formatted_ontology,
-        )
-
-        return prompt
-
-    @staticmethod
-    def _format_demonstrations(
-        demonstration_sentences: list[tuple[str, list[tuple[str, Polarity]]]]
-    ) -> str | None:
-
-        if not demonstration_sentences:
-            return None
-
-        return "\n".join(
-            [
-                f"Sentence: {sentence}\nAspects and Polarities: {[(aspect, str(polarity)) for aspect, polarity in aspects_and_polarities]}"
-                for sentence, aspects_and_polarities in demonstration_sentences
-            ]
-        )
+        return None
 
     @staticmethod
     def _build_prompt(
@@ -163,14 +182,13 @@ if __name__ == "__main__":
         demonstration_selection_method=DemonstrationSelectionMethod.SimCSE,
         top_k=3,
         train_data_filepath=os.getenv("PATH_TO_PREPROCESSED_SEMEVAL_15_RESTAURANTS_TRAIN_DATA"),
-        ontology_selection_method=OntologySelectionMethod.Partial,
+        ontology_selection_method=OntologySelectionMethod.Full,
         ontology_filepath=os.getenv("PATH_TO_RESTAURANT_ONTOLOGY"),
         ontology_format=OntologyFormat.XML,
     )
 
     print(prompt)
     exit()
-
     # for domain in ["laptop", "restaurant"]:
     #     for yeah in [2015, 2016]:
     #         for input_sentence, aspect in training_sentences:
